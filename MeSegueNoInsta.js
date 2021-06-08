126e50b083f3d7fee277ccab8a4ee5736ccc87a5
Vitor Leal <vitorlealm.21@gmail.com>
2021-06-08 20:54:57 -0300

    Exercicio de médio nivel do PSEL. Frase do blogueirinho

---
+++ MeSegueNoInsta.js
@@ -0,0 +1,13 @@
+
+function solucao(largura, altura) {
+    if (altura > largura){
+        console.log ("RETRATO");
+    }
+     else if (altura < largura){
+        console.log ("PAISAGEM");
+    }
+     else{
+        console.log ("QUADRADA");
+    }
+  }
+  
